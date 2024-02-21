4feecb68b989c2264c9f9ec4f3351f31055e4c91
Javier Carrasco <javierdeveloper08@gmail.com>
2024-02-21 11:26:20 -0400

    feat: Adding feature to filter categories

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -2,14 +2,21 @@ import stylesScss from '../styles/category.module.scss';
 
 interface Props {
   categories: string[];
+  onEventChange: (e: string) => void;
 }
 
-export default function Category({ categories }: Props) {
+export default function Category({ categories, onEventChange }: Props) {
   return (
-    <select>
+    <select
+      className={stylesScss.select}
+      onChange={(e) => {
+        onEventChange(e.target.value);
+      }}>
       <option defaultChecked>All</option>
       {categories.map((category: string, index) => (
-        <option key={index}>{category}</option>
+        <option key={index} value={category}>
+          {category}
+        </option>
       ))}
     </select>
   );
